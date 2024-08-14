e15a610030e2fdb5132149a008278cd030eea4a2
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-14 09:20:40 +0500

    elements inject

--- customReact.js
+++ customReact.js
@@ -1,10 +1,6 @@
 function customRender(reactElement, container){
     // Method #1
-    // const domElement = document.createElement(reactElement.type);
-    // domElement.innerHTML = reactElement.children;
-    // domElement.setAttribute('href',reactElement.props.href);
-    // domElement.setAttribute('target',reactElement.props.target);
-    // container.appendChild(domElement);
+    // const dom .appendChild(domElement);
 
     // Method #2
     const domElement = document.createElement(reactElement.type);
